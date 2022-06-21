84a3946edd65686e738f206bb7e21524f4337d1a
nahuel <nahuelblangetti@gmail.com>
2022-06-21 20:02:31 -0300

    fix formulario

--- js/contact.js
+++ js/contact.js
@@ -1,35 +1,15 @@
-function isMobile() {
-    if (sessionStorage.desktop)
-        return false;
-    else if (localStorage.mobile)
-        return true;
-    var mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
-    for (var i in mobile)
-        if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
-    return false;
-}
+const frm = document.querySelector("#frm");
 
-const formulario = document.querySelector('#formulario');
-const buttonSubmit = document.querySelector('#submit');
-const urlDesktop = 'https://web.whatsapp.com/';
-const urlMobile = 'whatsapp://';
-const telefono = '543512293555';
+frm.addEventListener("submit", event => {
+  event.preventDefault();
+  const frm = event.target;
+  const formData = {
+    name: frm.name.value,
+    email: frm.email.value,
+    date: frm.mensaje.value
+  };
 
-formulario.addEventListener('submit', (event) => {
-    event.preventDefault()
-    buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
-    buttonSubmit.disabled = true
-    setTimeout(() => {
-        let nombre = document.querySelector('#nombre').value
-        let apellidos = document.querySelector('#apellidos').value
-        let email = document.querySelector('#email').value
-        let mensaje = 'send?phone=' + telefono + '&text=*_Formulario Easy App CODE_*%0A*¿Cual es tu nombre?*%0A' + nombre + '%0A*¿Cuál es tu correo electrónico?*%0A' + apellidos + '%0A*Mensaje:*%0A' + email + ''
-        if(isMobile()) {
-            window.open(urlMobile + mensaje, '_blank')
-        }else{
-            window.open(urlDesktop + mensaje, '_blank')
-        }
-        buttonSubmit.innerHTML = '<i class="fa fa-whatsapp"></i> Enviar Mensaje'
-        buttonSubmit.disabled = false
-    }, 3000);
+  const URL = `https://api.whatsapp.com/send?Nombre=${formData.name}%20Email=${formData.email}%20Mensaje=${formData.date}`;
+
+  window.open(URL, "_blank");
 });
