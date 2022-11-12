7d0468e9dd138161a77f60980e24ef9e2e732b87
nahuel <nahuelblangetti@gmail.com>
2022-11-12 13:43:57 -0300

    fix portafolio complete

---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Leonardo Nahuel Blangetti",
+    "role": "Desarrollador Web Full Stack",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "con pasión hacía la programación que me ayuda a atravesar obstáculos, desafíos y aprender facilmente sobre nuevas tecnologías.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/nahuelblangetti/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://www.linkedin.com/in/nahuelblangetti/",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"Destaco mucho mi habilidad de amoldarme a las tecnologías gracias a mi gran conocimiento en JavaScript (lenguaje que utilizó hace más de 2 años), por eso puedo decir que tengo la agilidad de aprender a utilizar herramientas como React, Angular, Node JS (con las cuales trabaje). En la empresa Key Data me contratan siendo un desarrollador inexperimentado y tuve la oportunidad de desarrollar en el área de Back End con el lenguaje de programación PHP, framework Laravel y Base de Datos. Fue un desafío en el que aprendí como afrontarse hacia nuevas tecnologías y lo más importante la comunicación con mis compañeros e investigar sobre los desconocimientos que se me dispongan en el momento de trabajar.",
+    "address":"Argentina",
+    "website":"NahuelBlangetti@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Instituto Nuestra Señora de Fátima",
+        "specialization":"Nivel Secundario Completo",
+        "YearOfPassing":"2019"
+      },
+      {
+        "UniversityName":"Universidad Tecnológica Nacional",
+        "specialization":"Ingenieria en Sistemas de Informacion",
+        "YearOfPassing":"2021",
+        "Achievements":"Destacó mis años estudiados ya que es donde aprendí a programar con Python."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Key Data",
+        "specialization":"Desarrollador Web Full Stack",
+        "añoEntrada":"Agosto 2021",
+        "YearOfLeaving":"Actualidad",
+        "Achievements":"Creación y actualización de modulos en el area de Front y Back"
+      }
+    ],
+    "skillsDescription":"Herramientas en las que tengo experiencia",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"PHP"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Angularjs"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Laravel"
+      },
+      {
+        "skillname":"Livewire"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"MongoDB"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Gallery App",
+        "description":"Aplicacion para buscar imagenes aleatorias realizadas con Reactjs",
+        "imgurl":"images/portfolio/cars.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Espero haber obtenido una buena referencia sobre mi.",
+        "name":"Leonardo Nahuel Blangetti"
+      }
+    ]
+  }
+  
+  export default resumeData
