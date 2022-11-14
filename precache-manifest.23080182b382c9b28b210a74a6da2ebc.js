b27d86ef69a9c33767057c1940e2f7e6eb69ac5c
nahuel <nahuelblangetti@gmail.com>
2022-11-14 19:52:03 -0300

    Updates

---
+++ precache-manifest.23080182b382c9b28b210a74a6da2ebc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a299c328536b048a2475",
+    "url": "/Portafolio/static/css/main.212ee611.chunk.css"
+  },
+  {
+    "revision": "a299c328536b048a2475",
+    "url": "/Portafolio/static/js/main.a299c328.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Portafolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "f5da09a35f7e7a7ea3e3",
+    "url": "/Portafolio/static/js/runtime~main.f5da09a3.js"
+  },
+  {
+    "revision": "ab3edcf24d8d33cf36c7db596f4c5183",
+    "url": "/Portafolio/index.html"
+  }
+];
